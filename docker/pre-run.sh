4a4f15ee3869e88a9c1ece8c4528ecc9ea02a933
OllieJC <5426038+OllieJC@users.noreply.github.com>
2025-08-03 00:03:29 +0100

    Debug pre-run

--- docker/pre-run.sh
+++ docker/pre-run.sh
@@ -14,13 +14,15 @@ else
     cp /app/views/index.html.bak /app/views/index.html
 fi
 
-if [ -z "$CUSTOM_TITLE" ]; then
+if [ -z "${CUSTOM_TITLE:-}" ]; then
     CUSTOM_TITLE="PLANKA by GDS"
 fi
+echo "pre-run.sh: Setting custom title to: $CUSTOM_TITLE"
 sed -i "s%<title>PLANKA</title>%<title>$CUSTOM_TITLE</title>%" /app/public/index.html
 sed -i "s%<title>PLANKA</title>%<title>$CUSTOM_TITLE</title>%" /app/views/index.html
 
 VERSION_TIMESTAMP=$(date +%s)
+echo "pre-run.sh: Setting version timestamp to: $VERSION_TIMESTAMP"
 sed -i "s| </head>|<link rel=\"stylesheet\" crossorigin href=\"/assets/custom.css?v=$VERSION_TIMESTAMP\"></head>|" /app/public/index.html
 sed -i "s| </head>|<link rel=\"stylesheet\" crossorigin href=\"/assets/custom.css?v=$VERSION_TIMESTAMP\"></head>|" /app/views/index.html
 
@@ -31,17 +33,22 @@ else
     cp /app/public/assets/custom.css.bak /app/public/assets/custom.css
 fi
 
-if [ -z "$CUSTOM_LOGIN_TAG" ]; then
+if [ -z "${CUSTOM_LOGIN_TAG:-}" ]; then
     CUSTOM_LOGIN_TAG=""
 fi
-sed -i "s%LOGIN_TAG%$CUSTOM_LOGIN_TAG%" /app/public/assets/custom.css
+echo "pre-run.sh: Setting custom login tag to: $CUSTOM_LOGIN_TAG"
+sed -i "s%CUSTOM_LOGIN_TAG%$CUSTOM_LOGIN_TAG%" /app/public/assets/custom.css
 
-if [ -z "$CUSTOM_LOGIN_TITLE" ]; then
+if [ -z "${CUSTOM_LOGIN_TITLE:-}" ]; then
     CUSTOM_LOGIN_TITLE="PLANKA"
 fi
-sed -i "s%LOGIN_TITLE%$CUSTOM_LOGIN_TITLE%" /app/public/assets/custom.css
+echo "pre-run.sh: Setting custom login title to: $CUSTOM_LOGIN_TITLE"
+sed -i "s%CUSTOM_LOGIN_TITLE%$CUSTOM_LOGIN_TITLE%" /app/public/assets/custom.css
 
-if [ -z "$CUSTOM_APP_TITLE" ]; then
+if [ -z "${CUSTOM_APP_TITLE:-}" ]; then
     CUSTOM_APP_TITLE="PLANKA"
 fi
-sed -i "s%APP_TITLE%$CUSTOM_APP_TITLE%" /app/public/assets/custom.css
+echo "pre-run.sh: Setting custom app title to: $CUSTOM_APP_TITLE"
+sed -i "s%CUSTOM_APP_TITLE%$CUSTOM_APP_TITLE%" /app/public/assets/custom.css
+
+echo "pre-run.sh: completed successfully."
